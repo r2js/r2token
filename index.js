31121efaebcfd3f6570e59ad7683806fd4446cf5
Selçuk Fatih Sevinç <airsfs@gmail.com>
2017-11-13 21:46:54 +0300

    v1.0.1
    * config fix

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const modelToken = require('./model/token');
 const log = require('debug')('r2:token');
 
 module.exports = function Token(app, conf) {
-  const getConf = conf || app.config('apn');
+  const getConf = conf || app.config('jwt');
   if (!getConf) {
     return log('jwt config not found!');
   }
 
   const mToken = modelToken(app);
   const { secret } = getConf;
+  const { getToken, decodeToken } = app.utils;
 
   return {
     create(data = {}) {
       const { email = 'test@r2js.org', type = 'login' } = data;
-      const { token } = app.utils.getToken({ email, type }, secret);
+      const { token } = getToken({ email, type }, secret);
       const saveObj = { email, type, token };
       return mToken.findOrCreate(
-        saveObj, Object.assign({}, saveObj, { data: _.omit(data, ['email', 'type']) })
+        saveObj, Object.assign({}, saveObj, { data: _.omit(data, 'email', 'type') })
       );
     },
 
@@ -25,7 +26,7 @@ module.exports = function Token(app, conf) {
       let tokenData;
       return mToken.findOneOrError({ token })
         .then((data) => {
-          const decoded = app.utils.decodeToken(data.token, secret);
+          const decoded = decodeToken(data.token, secret);
           if (type !== data.type || type !== decoded.type) {
             return Promise.reject('wrong token type!');
           }
